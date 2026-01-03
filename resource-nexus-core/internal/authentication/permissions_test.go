8484fc0ddc428921e90e25a374ca65b202626034
Tobias <tobias@bauriedel.de>
2026-01-03 23:52:43 +0100

    resource-nexus-core - add tests for permissions

--- resource-nexus-core/internal/authentication/permissions_test.go
+++ resource-nexus-core/internal/authentication/permissions_test.go
@@ -1,6 +1,38 @@
 package authentication
 
-import "testing"
+import (
+	"testing"
+)
+
+func TestPermissions(t *testing.T) {
+	// get all permissions and check return type
+	var p map[string]string
+	p = permissions()
+
+	if len(p) == 0 {
+		t.Fatal("permissions map is empty")
+	}
+
+	if permissions()["/system/health"] != "system:health:get" {
+		t.Fatal("wrong permission returned")
+	}
+}
+
+func TestGetPermissionForPath(t *testing.T) {
+	p, ok := GetPermissionForPath("/system/health")
+	if !ok {
+		t.Fatal("permission not found")
+	}
+
+	if p != "system:health:get" {
+		t.Fatal("wrong permission returned")
+	}
+
+	p, ok = GetPermissionForPath("/system/health2")
+	if ok {
+		t.Fatal("permission found for non existing path")
+	}
+}
 
 func TestBuildPermissionString(t *testing.T) {
 	actual := BuildPermissionString("security", "user", "create")
